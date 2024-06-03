df4cbbac7ce15725512ca437ed47716eea660c47
Mr robot <44882892+gwanghong@users.noreply.github.com>
2024-06-03 14:01:09 -0600

    GameTest

--- server/src/test/java/service/GameTest.java
+++ server/src/test/java/service/GameTest.java
@@ -4,9 +4,8 @@ import dataaccess.DataAccessException;
 import dataaccess.*;
 import model.AuthData;
 import model.GameData;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import model.UserData;
+import org.junit.jupiter.api.*;
 import service.GameService;
 import service.UserService;
 
@@ -17,28 +16,81 @@ public class GameTest {
     private static GameService gameService;
     private static GameDAO gameDao;
     private static AuthDAO authDao;
-    private static AuthData authData;
+    private static GameData game;
 
-    @BeforeEach
-    void setUp() throws DataAccessException {
+
+    @BeforeAll
+    static void setUp() throws DataAccessException {
         gameDao = new MemoryGameDAO();
         authDao = new MemoryAuthDAO();
         gameService = new GameService(gameDao, authDao);
-
-        AuthData authD = new AuthData("testAuthToken", "testUser");
-        authDao.createAuth(authD);
-
     }
 
     @Test
     @DisplayName("Valid request / return success")
     public void positiveTest() throws Exception {
-        //List<GameData> games = gameService.listGames(authDao.getAuth());
+        AuthData auth = new AuthData("1234", "newU");
+        authDao.createAuth(auth);
+
+        try {
+            gameService.isAuthTokenValid("1234");
+        } catch (DataAccessException e) {
+            Assertions.assertTrue(false);
+        }
+
+        Assertions.assertEquals(0, gameService.listGames(auth.authToken()).size());
+        GameData createGame = gameService.createGame(auth.authToken(), "newGame");
+        Assertions.assertNotNull(createGame);
+        Assertions.assertEquals(1, gameService.listGames(auth.authToken()).size());
+
+        gameService.joinGame("1234", "WHITE", createGame.gameID());
+        Assertions.assertEquals("newU", gameDao.getGame(createGame.gameID()).whiteUsername());
     }
 
     @Test
     @DisplayName("Invalid request / return failure")
     public void negativeTest() throws DataAccessException {
+        AuthData auth = new AuthData("1234", "newU");
 
+        try {
+            gameService.createGame("1365", "newGame");
+
+            Assertions.assertTrue(false);
+        } catch (DataAccessException e) {
+            Assertions.assertTrue(true);
+        }
+
+        try {
+            gameService.listGames("1365");
+
+            Assertions.assertTrue(false);
+        } catch (DataAccessException e) {
+            Assertions.assertTrue(true);
+        }
+
+        GameData game = gameService.createGame("1234", "new");
+        try {
+            gameService.joinGame("1365", "WHITE", game.gameID());
+
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.assertTrue(true);
+        }
+
+        try {
+            gameService.joinGame("1234", "GREY", game.gameID());
+
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.assertTrue(true);
+        }
+
+        try {
+            gameService.joinGame("1234", "BLACK", 568);
+
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.assertTrue(true);
+        }
     }
 }
