5a7527aa38c24e4e87da35ea172ab90c9a059816
Mr robot <44882892+gwanghong@users.noreply.github.com>
2024-06-10 00:41:56 -0600

    added tictactoe board to see how code works. Working on DisplayBoard

--- client/src/main/java/ui/DisplayBoard.java
+++ client/src/main/java/ui/DisplayBoard.java
@@ -1,4 +1,16 @@
 package ui;
 
 public class DisplayBoard {
+
+    private static final int BOARD_SIZE_IN_SQUARES = 10;
+    private static final int SQUARE_SIZE_IN_CHARS = 3;
+    private static final int LINE_WIDTH_IN_CHARS = 1;
+    private static final String EMPTY = "   ";
+
+    private static void drawBoard() {
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+
+
+        }
+    }
 }
