681c6705907dfb2575631c0707512337fba6b741
Mr robot <44882892+gwanghong@users.noreply.github.com>
2024-06-11 22:49:43 -0600

    got rid of unnecessary method blocks

--- client/src/main/java/ui/DisplayBoard.java
+++ client/src/main/java/ui/DisplayBoard.java
@@ -17,39 +17,7 @@ public class DisplayBoard {
 
         out.print(ERASE_SCREEN);
 
-        //drawHeaders(out);
-
         drawBoard(out);
-
-        //drawHeaders(out);
-
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_WHITE);
-    }
-
-    private static void drawHeaders(PrintStream out) {
-
-        setGrey(out);
-
-        String[] headers = {" ", "a", "b", "c", "d", "e", "f", "g", "h", " "};
-        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            drawHeader(out, headers[boardCol]);
-
-            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-            }
-        }
-
-        out.println();
-    }
-
-    private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-
-        out.print(EMPTY.repeat(prefixLength));
-        printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
     }
 
     private static void printHeaderText(PrintStream out, String player) {
@@ -114,20 +82,6 @@ public class DisplayBoard {
                 setBlack(out);
             }
 
-            out.println();
-        }
-    }
-
-    private static void drawVerticalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_CHARS; ++lineRow) {
-            setGrey(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
             out.println();
         }
     }
