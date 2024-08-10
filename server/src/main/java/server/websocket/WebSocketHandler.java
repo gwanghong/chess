decfe8243f3aecd18064eb10b4f4526853487713
Mr robot <44882892+gwanghong@users.noreply.github.com>
2024-08-10 13:51:57 -0600

    overall structure for websockethandler done

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,51 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class WebSocketHandler {
+
+    Map<Integer, Set<Session>> connections = new HashMap<>();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) {
+        UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+        switch (userGameCommand.getCommandType()) {
+            case CONNECT -> connect();
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect() {
+    }
+
+    private void makeMove() {
+    }
+
+    private void leave() {
+    }
+
+    private void resign() {
+    }
+
+    @OnWebSocketClose
+    public void onClose(int status, String message) {
+
+    }
+
+    @OnWebSocketError
+    public void onError(Throwable e) {
+
+    }
 }
